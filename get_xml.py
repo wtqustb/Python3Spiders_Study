19f96e15c1c6efbcd1202f20034e994894beb412
tq <44996892+wtqustb@users.noreply.github.com>
2020-04-29 09:53:48 +0800

    Update get_xml.py

--- get_xml.py
+++ get_xml.py
@@ -1,15 +1,23 @@
 import requests
 import json
 
-url = "http://118.184.217.73:7182/ReportStudent/SLabStatisticPage/GetLabStatisticList"
-headers={
-    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36",
-    "Referer":"http://118.184.217.73:7182/ReportStudent/SLabStatisticPage/GetLabStatisticList",
-    "Cookie":"COOKIES_KEY_USERNAME=41824089; ASP.NET_SessionId=1aoqcu240l1a2345r34tfa45"
-              
+url = "http://118.184.217.73:7182/Login/UserLogin"
+
+headers = {
+    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36"
 }
 
 data = {
+    "userId": "NDE4MjQwODk=",
+    "userPass": "d2FuZ3RpYW5xaTg4NjY="
+}
+
+s = requests.Session()
+resq = s.post(url=url,headers=headers,data=data)
+
+posturl = "http://118.184.217.73:7182/ReportStudent/SLabStatisticPage/GetLabStatisticList"
+
+postdata = {
     "LABTYPEID": -999,
     "LABNAME":"", 
     "ClassName":"", 
@@ -17,32 +25,9 @@ data = {
     "pageSize": 10,
 
 }
-
 base_url = "http://118.184.217.73:7182/"
-
-def get_response():
-    response = requests.post(url=url, headers=headers,data=data)
-
-    try:
-        if response.status_code == 200:
-            return response.text
-        else:
-            print(response.status_code)
-    except BaseException as e:
-        print(e.args)
-
-
-def get_xml(response):
-    html = json.loads(response)
-    datalists = html["DataList"]
-    for datalist in datalists:
-        print(base_url+datalist["LabDateUrl"])
-        
-
-
-
-if __name__ == "__main__":
-    get_xml(get_response())
-
-
-
+response = s.post(url=posturl,headers=headers,data=postdata)
+html = json.loads(response.text)
+datalists = html["DataList"]
+for datalist in datalists:
+    print(base_url+datalist["LabDateUrl"])
